9f7a47aef1b6ef979372e0b70349b5703085431c
pedrobolfute <pepenike-101@hotmail.com>
2024-12-18 23:22:34 -0300

    adicionado função que calcula metros quadrado para centimetro quadrado

--- tarefa/main.c
+++ tarefa/main.c
@@ -2,11 +2,14 @@
 //cabecalhos de apoio ao compilador
 float converte_cm_m(float);
 float converte_mm_m(float);
+float converter_m_quadrado_cm_quadrado(float);
 
 int main() {
     
     printf("\nResultado: %.2f metros.", converte_cm_m(120));  //chamada da funcao que converte de centimetros para metros.
     printf("\nResultado: %.2f metros.", converte_mm_m(1700));  //chamada da funcao que converte de milimetros para metros.
+    printf("\nResultado: %.2f centimetro quadrado.\n\n", converter_m_quadrado_cm_quadrado(1.15));  //chamada da funcao que converte de metro quadrado para centimetro quadrado.
+
 
     return 0;
 }
@@ -29,4 +32,9 @@ float converte_mm_m(float mm){
 
     return resultado;  //retorna o resultado
 
+}
+
+//Autor: Pedrobolfute
+float converter_m_quadrado_cm_quadrado(float valor){
+    return valor*10000;
 }
