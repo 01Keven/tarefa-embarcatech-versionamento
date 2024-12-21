a7b84679f45479da5b1c9d67274361575fea208d
Wilton Júnior <wiltonjrtablet@gmail.com>
2024-12-21 13:14:43 -0300

    formatacao da funcao de converter bits para padrao antes utilizado

--- tarefa/main.c
+++ tarefa/main.c
@@ -4,6 +4,39 @@
 //cabecalhos de apoio ao compilador
 float converte_cm_m(float);
 float converte_mm_m(float);
+long double converterBits(double, short int, short int);
+
+
+int main() {
+    
+    UnidadeTempo();
+
+
+    printf("\nResultado: %.2f metros.", converte_cm_m(120));  //chamada da funcao que converte de centimetros para metros.
+    printf("\nResultado: %.2f metros.", converte_mm_m(1700));  //chamada da funcao que converte de milimetros para metros.
+
+    return 0;
+}
+//Autor: Hercules Sampaio Oliveira
+float converte_cm_m(float cm){
+
+    float resultado;
+
+    resultado = cm / 100;  //realiza o calculo para metros
+
+    return resultado;  //retorna o resultado
+
+}
+//Autor: Hercules Sampaio Oliveira
+float converte_mm_m(float mm){
+
+    float resultado;
+
+    resultado = mm / 1000;  //realiza o calculo para metros
+
+    return resultado;  //retorna o resultado
+
+}
 long double converterBits(double numero, short int unidade, short int saida) // saida em long double, tres entradas. Funcao por Wilton
 {
     // entrada: primeiro o numero para ser convertido, segundo a unidade de conversao (tipos na proxima linha), terceiro tipo de saida desejada
@@ -63,35 +96,4 @@ long double converterBits(double numero, short int unidade, short int saida) // 
         break;
     }
     return result; // retorna o resultado
-}
-
-int main() {
-    
-    UnidadeTempo();
-
-
-    printf("\nResultado: %.2f metros.", converte_cm_m(120));  //chamada da funcao que converte de centimetros para metros.
-    printf("\nResultado: %.2f metros.", converte_mm_m(1700));  //chamada da funcao que converte de milimetros para metros.
-
-    return 0;
-}
-//Autor: Hercules Sampaio Oliveira
-float converte_cm_m(float cm){
-
-    float resultado;
-
-    resultado = cm / 100;  //realiza o calculo para metros
-
-    return resultado;  //retorna o resultado
-
-}
-//Autor: Hercules Sampaio Oliveira
-float converte_mm_m(float mm){
-
-    float resultado;
-
-    resultado = mm / 1000;  //realiza o calculo para metros
-
-    return resultado;  //retorna o resultado
-
 }
