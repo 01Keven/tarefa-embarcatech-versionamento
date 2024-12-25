9e38fd390b918604adb3c3cf9eca135211f67509
HerculesDraycon <herculescarros2@gmail.com>
2024-12-25 20:02:43 -0300

    refatorei a parte de unidades de comprimento do código, limpando a main como foi pedido.

---
+++ tarefa/lib/comprimento.h
@@ -0,0 +1,9 @@
+#ifndef MEDIDA_COMPRIMENTO
+#define MEDIDA_COMPRIMENTO
+
+void unidadeComprimento();
+void metros(float num);
+void centimetros(float num);
+void milimetros(float num);
+
+#endif
--- tarefa/main.c
+++ tarefa/main.c
@@ -1,43 +1,19 @@
 #include <stdio.h>
 #include "lib/tempo.h"
+#include "lib/comprimento.h"
+#include "lib/temperatura.h"
 #include "lib/converter_m_quadrado_cm_quadrado.h"
 
-#include "lib/temperatura.h"
 
-//cabecalhos de apoio ao compilador
-float converte_cm_m(float);
-float converte_mm_m(float);
 float converter_m_quadrado_cm_quadrado(float);
 
 int main() {
+
+    unidadeComprimento();
     unidadeTemperatura();
     UnidadeTempo();
 
-
-    printf("\nResultado: %.2f metros.", converte_cm_m(120));  //chamada da funcao que converte de centimetros para metros.
-    printf("\nResultado: %.2f metros.", converte_mm_m(1700));  //chamada da funcao que converte de milimetros para metros.
     printf("\nResultado: %.2f centimetro quadrado.\n\n", converter_m_quadrado_cm_quadrado(1.15));  //chamada da funcao que converte de metro quadrado para centimetro quadrado.
 
-
     return 0;
-}
-//Autor: Hercules Sampaio Oliveira
-float converte_cm_m(float cm){
-
-    float resultado;
-
-    resultado = cm / 100;  //realiza o calculo para metros
-
-    return resultado;  //retorna o resultado
-
-}
-//Autor: Hercules Sampaio Oliveira
-float converte_mm_m(float mm){
-
-    float resultado;
-
-    resultado = mm / 1000;  //realiza o calculo para metros
-
-    return resultado;  //retorna o resultado
-
 }
